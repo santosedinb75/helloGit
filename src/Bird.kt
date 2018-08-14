5ea33445ef827b26fd65ff9fc71c70338ad045b1
Mike Santos <santosedinb75@gmail.com>
2018-08-13 23:06:04 -0500

    Add bird class

---
+++ src/Bird.kt
@@ -0,0 +1,11 @@
+class Bird(name: String) {
+    val name = name
+
+    fun getBreed(): String {
+        return "Parakeet"
+    }
+
+    fun tweet() {
+        println("Tweet!")
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,11 @@
 fun main(args: Array<String>) {
     val fido = Dog()
     val whiskers = Cat("Whiskers")
+    val tweety = Bird("Tweety")
     println("Fido is ${fido.getColor()}")
     fido.bark()
     println("${whiskers.getName()} is ${whiskers.getColor()}")
     whiskers.meow()
+    println( "${tweety.name} is a ${tweety.getBreed()}")
+    tweety.tweet()
 }
